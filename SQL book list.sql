02078d46f505538c0457d34979f124ce9b7f9aac
PrathiChiku <86074406+PrathibhaSithu@users.noreply.github.com>
2021-06-18 20:55:16 +0530

    Create SQL book list.sql

---
+++ SQL book list.sql
@@ -0,0 +1,6 @@
+CREATE TABLE BookList (id INTEGER, name TEXT, rating INTEGER );
+
+INSERT INTO BookList VALUES (1, "Harry Potter", 7);
+INSERT INTO BookList VALUES (2, "After", 5);
+INSERT INTO BookList VALUES (3, "Avatar the last air bender", 4);
+SELECT * FROM BookList;
